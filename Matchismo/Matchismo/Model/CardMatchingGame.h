ca9ff940cd640a33281a73909ed5b042e973304f
Stephanie New <snew@domeyard.com>
2015-06-05 10:38:06 -0400

    Updated with Lecture 3 changes

---
+++ Matchismo/Matchismo/Model/CardMatchingGame.h
@@ -0,0 +1,23 @@
+//
+//  CardMatchingGame.h
+//  Matchismo
+//
+//  Created by Stephanie New on 6/5/15.
+//  Copyright (c) 2015 Domeyard. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Deck.h"
+#import "Card.h"
+
+@interface CardMatchingGame : NSObject
+
+// designated initializer
+- (instancetype)initWithCardCount:(NSUInteger)count
+                             usingDeck:(Deck *)deck;
+- (void)chooseCardAtIndex:(NSUInteger)index;
+- (Card *)cardAtIndex:(NSUInteger)index;
+
+@property (nonatomic, readonly) NSInteger score;
+
+@end
--- Matchismo/Matchismo/Model/PlayingCard.m
+++ Matchismo/Matchismo/Model/PlayingCard.m
@@ -13,6 +13,21 @@
 // Class methods start with +
 // Instance methods start with -
 
+- (int)match:(NSArray *)otherCards
+{
+    int score = 0;
+    
+    if ([otherCards count] == 1) {
+        PlayingCard *otherCard = [otherCards firstObject];
+        if (otherCard.rank == self.rank) {
+            score = 4;
+        } else if ([otherCard.suit isEqualToString:self.suit]) {
+            score = 1;
+        }
+    }
+    return score;
+}
+
 - (NSString *) contents
 {
     NSArray *rankStrings = [PlayingCard rankStrings];
